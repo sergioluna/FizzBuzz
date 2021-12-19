c2f51d38d466cb40555b7dd7abdb4f5324e4bdb7
sergioluna <sluna@sergioluna.dev>
2021-12-19 14:40:51 -0800

    Create function definitions

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 def main():
     pass
 
+def fizzbuzz(start: int, end: int):
+    """
+    Prints out all numbers in range start to end while appending Fizz to
+    perfect squares, Buzz to prime numbers, and FizzBuzz to numbers that are
+    both perfect squares and prime numbers.
+    """
+    pass
+
+def is_perfect_square(num: int) -> bool:
+    """
+    Determine whether num is a perfect square or not
+    """
+    pass
+
+def is_prime_num(num: int) -> bool:
+    """
+    Determine whether num is a prime number or not
+    """
+    pass
+
 if __name__ == '__main__': 
     main()
