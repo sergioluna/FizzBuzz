06ce995a8f95a6e499f71e738267c14009620e52
sergioluna <sluna@sergioluna.dev>
2021-12-19 15:24:44 -0800

    Implement is_prime function

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import math
 def main():
     fizzbuzz(2, 144)
 
+
 def fizzbuzz(start: int, end: int):
     """
-    Prints out all numbers in range start to end while appending 'fizz' to
+    Prints out all numbers from start to end while appending 'fizz' to
     perfect squares, 'buzz' to prime numbers, and 'fizzbuzz' to numbers that 
     are both perfect squares and prime numbers.
     """
@@ -27,14 +28,22 @@ def is_perfect_square(num: int) -> bool:
     """
     Returns True if num is a perfect square, False otherwise
     """
+    # math.isqrt() returns floor of exact square root of num
     return (math.isqrt(num) ** 2 == num) 
 
 
 def is_prime(num: int) -> bool:
     """
-    Determine whether num is a prime number or not
+    Returns True if num is prime, False otherwise
     """
-    pass
+    if num <= 1:
+        return False
+    else:
+        # iterate through relevant factors to check if num is divisible
+        for factor in range(2, math.isqrt(num) + 1):
+            if num % factor == 0:
+                return False
+        return True
 
 
 if __name__ == '__main__': 
