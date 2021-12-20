3dd66a53e9fba23812f560c5fa5dd5faf6a636f9
sergioluna <sluna@sergioluna.dev>
2021-12-19 16:06:16 -0800

    Refactor tests.py for cleaner code

--- tests.py
+++ tests.py
@@ -2,9 +2,13 @@ import unittest
 
 from main import is_perfect_square, is_prime
 
+
 class TestFizzBuzz(unittest.TestCase):
 
     def test_is_prime_from_2_to_144(self):
+        """
+        Test main.is_prime from 2 to 144
+        """
         prime_numbers = {
             2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
             47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
@@ -17,8 +21,10 @@ class TestFizzBuzz(unittest.TestCase):
             else:
                 self.assertFalse(is_prime(n))
 
-
     def test_is_perfect_square_from_2_to_144(self):
+        """
+        Test main.is_perfect_square from 2 to 144
+        """
         perfect_squares = {
             4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144
         }
@@ -28,6 +34,7 @@ class TestFizzBuzz(unittest.TestCase):
                 self.assertTrue(is_perfect_square(n))
             else:
                 self.assertFalse(is_perfect_square(n))
+
 
 if __name__ == '__main__':
     unittest.main()
